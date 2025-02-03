c7692062564ed9e36dcf5c6ab738ae0ee3004863
TacioNeves <tacionevesdasilva@gmail.com>
2025-02-03 09:58:51 -0800

    updated of contact, socail media at footer section

--- frontend/src/components/Footer/Footer.js
+++ frontend/src/components/Footer/Footer.js
@@ -6,10 +6,16 @@ const Footer = () => {
     <footer className="bg-dark text-light">
       <div className="container footer-sec">
         <div className="copy-right-sec d-flex justify-content-between">
+          <div>
+            <p className="copy-right-title-sec">Contact</p>
+            <b>RUDINE</b> : Matale, Central 21000, LK<br/>
+            <b>Email</b> : manager@rudine.pro
+          </div>
           <p className="copy-right">&copy; 2024 Veeramanikandan. All Rights Reserved.
-</p>
+          </p>
           <div>
             {/* Social Media Logos */}
+            <p className='copy-right-title-sec'>Social Media</p>
             <img className="social-logo"
               src="https://res.cloudinary.com/dpcfyn3si/image/upload/v1706617641/Wabeler/ktzoghgtys5x4al7o3wb.png"
               alt="social-logo" />
